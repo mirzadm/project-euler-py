78218abb18c6a768efc025d2cb651285258101d6
Mirzad Mohandespour <mirzadm@gmail.com>
2018-07-13 16:49:29 -0500

    problem 3

---
+++ src/p3.py
@@ -0,0 +1,53 @@
+"""The largest prime factor of n."""
+
+from math import sqrt
+
+
+def is_prime_wrt_factors(m, primes):
+    """Checks primality of m with respect to given prime factors."""
+    m_sqrt = int(sqrt(m))
+    is_prime = True
+    i = 0
+    while i < len(primes) and primes[i] <= m_sqrt and is_prime:
+        if m % primes[i] == 0:
+            is_prime = False
+        i += 1
+    return is_prime
+
+
+def prime_numbers_up_to(x):
+    """Returns list of prime numbers <= x.
+    
+    Uses `is_prime_wrt_factors`.
+    """
+    primes = []
+    for i in range(2, x+1):
+        if is_prime_wrt_factors(i, primes):
+            primes.append(i)
+    return primes
+
+
+def largest_prime_factor(n):
+    """Find the largres prime factor of n."""
+    if n < 2:
+        return None
+    # Find all prime number <= sqrt(n)
+    primes = prime_numbers_up_to(int(sqrt(n)))
+    # Filter prime factors of n
+    prime_factors_subset = []
+    for p in primes:
+        if n % p == 0:
+            prime_factors_subset.append(p)
+    # Check primality of n
+    if prime_factors_subset == []:
+        return n
+    # Find maximum prime factor
+    i = 0
+    while i < len(prime_factors_subset) and n > 1:
+        while n % prime_factors_subset[i] == 0:
+            n = n // prime_factors_subset[i]
+        i += 1
+    if n > 1:
+        return n
+    else:
+        return prime_factors_subset[-1]
