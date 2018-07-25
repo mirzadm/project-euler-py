454fdbb879f023019e6abab6dc0b40a0dd2bcb66
Mirzad Mohandespour <mirzadm@gmail.com>
2018-07-25 12:52:00 -0500

    problem 11: add tests

---
+++ tests/test_p11.py
@@ -0,0 +1,30 @@
+"""Unit tests for p11.py."""
+
+import unittest
+
+from src.p11 import max_adj_prod
+
+
+class TestP11(unittest.TestCase):
+    
+    def test_max_adj_prod_exception(self):
+        grid = [[1, 2], [3, 4]]
+        self.assertRaises(ValueError, max_adj_prod, grid, 3)
+        self.assertRaises(ValueError, max_adj_prod, grid, 0)
+
+    def test_max_adj_prod_1_by_1(self):
+        grid = [[5]]
+        self.assertEqual(max_adj_prod(grid, 1), 5)
+
+    def test_max_adj_prod_2_by_2(self):
+        grid = [[4, 0], [2, 3]]
+        self.assertEqual(max_adj_prod(grid, 1), 4)
+        self.assertEqual(max_adj_prod(grid, 2), 12)
+        
+        grid = [[0, 4], [3, 2]]
+        self.assertEqual(max_adj_prod(grid, 1), 4)
+        self.assertEqual(max_adj_prod(grid, 2), 12)
+
+
+if __name__ == '__main__':
+    unittest.main()
