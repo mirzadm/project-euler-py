66d88feb2246f830422081a7752763a1ddd655ca
Mirzad Mohandespour <mirzadm@gmail.com>
2018-07-24 22:24:40 -0500

    problem 10: more efficient algorithm

--- src/p10.py
+++ src/p10.py
@@ -1,17 +1,39 @@
 """Project Euler: Problem 10."""
 
-from src.p7 import is_prime
+from math import sqrt
 
 
-def sum_of_primes(max_value):
-    """Finds sum of all prime number < `max_value`."""
-    primes = []
+def sum_of_primes(upper_limit):
+    """Finds sum of all prime numbers < `upper_limit`.
+    
+    Uses the method known as sieve of Eratosthenes.
+    
+    Args:
+        `upper_limit`: Positive integer. 
+    Returns:
+        Sum of all prime numbers less than `upper_limit`. 
+        0 if `upper_limit` less than 3.
+    Raises:
+        ValueError: If `upper_limit` less than 1.
+    """
+    if upper_limit < 1:
+        raise ValueError('Input argument must be greater than or equal to 1.')
+    
+    # Default to prime
+    # Numbers 0 and 1 are also included here but just to simplify
+    # index-number relation. They are not actually used.
+    is_prime_array = [True] * upper_limit  
+
+    upper_limit_sqrt = int(sqrt(upper_limit))
+    
+    for n in range(2, upper_limit_sqrt + 1):
+        if is_prime_array[n]:
+            for i in range(2 * n, upper_limit, n):
+                is_prime_array[i] = False
+    
     total = 0
-    candidate = 2
-    while candidate < max_value:
-        if is_prime(candidate, primes):
-            primes.append(candidate)
-            total += candidate
-        candidate += 1
+    for n in range(2, upper_limit):
+        if is_prime_array[n]:
+            total += n
 
     return total
