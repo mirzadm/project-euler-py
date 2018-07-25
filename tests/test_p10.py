0fa161dee8266a1d1bc4a39ad8c377900cc286d4
Mirzad Mohandespour <mirzadm@gmail.com>
2018-07-24 22:25:36 -0500

    problem 10: add tests

---
+++ tests/test_p10.py
@@ -0,0 +1,25 @@
+"""Unit tests for p10.py."""
+
+import unittest
+
+from src.p10 import sum_of_primes
+
+
+class TestP10(unittest.TestCase):
+
+    def test_exception(self):
+        self.assertRaises(ValueError, sum_of_primes, 0)
+
+    def test_expected(self):
+        self.assertEqual(sum_of_primes(1), 0)
+        self.assertEqual(sum_of_primes(2), 0)
+        self.assertEqual(sum_of_primes(3), 2)
+        self.assertEqual(sum_of_primes(4), 5)
+        self.assertEqual(sum_of_primes(5), 5)
+        self.assertEqual(sum_of_primes(6), 10)
+        self.assertEqual(sum_of_primes(7), 10)
+        self.assertEqual(sum_of_primes(8), 17)
+
+
+if __name__ == '__main__':
+    unittest.main()
