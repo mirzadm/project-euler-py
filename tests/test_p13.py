d0f111aee01efc91e200c9312a72462c9be36eac
Mirzad Mohandespour <mirzadm@gmail.com>
2018-07-29 21:35:39 -0500

    problem 13: tests for third version

--- tests/test_p13.py
+++ tests/test_p13.py
@@ -2,33 +2,36 @@
 
 import unittest
 
-from src.p13 import add_num_str
+from src.p13 import number_string_to_list, add_int_lists_with_carry
 
 
 class TestP13(unittest.TestCase):
 
-    def test_exception(self):
-        self.assertRaises(ValueError, add_num_str, '12', '34', 0)
+    def test_number_string_to_list_exception(self):
+        self.assertRaises(ValueError, number_string_to_list, '123', 0)
 
-    def test_expected(self):
-        self.assertEqual(add_num_str('', '', 1), '')
-        self.assertEqual(add_num_str('', '', 2), '')
-        self.assertEqual(add_num_str('999', '', 1), '999')
-        self.assertEqual(add_num_str('999', '', 2), '999')
-        self.assertEqual(add_num_str('999', '', 3), '999')
-        self.assertEqual(add_num_str('999', '', 4), '999')
-        self.assertEqual(add_num_str('999', '0', 1), '999')
-        self.assertEqual(add_num_str('999', '0', 2), '999')
-        self.assertEqual(add_num_str('999', '0', 3), '999')
-        self.assertEqual(add_num_str('999', '0', 4), '999')
-        self.assertEqual(add_num_str('999', '1', 1), '1000')
-        self.assertEqual(add_num_str('999', '1', 2), '1000')
-        self.assertEqual(add_num_str('999', '1', 3), '1000')
-        self.assertEqual(add_num_str('999', '1', 4), '1000')
-        self.assertEqual(add_num_str('999', '999', 1), '1998')
-        self.assertEqual(add_num_str('999', '999', 2), '1998')
-        self.assertEqual(add_num_str('999', '999', 3), '1998')
-        self.assertEqual(add_num_str('999', '999', 4), '1998')
+    def test_number_string_to_list_expected(self):
+        self.assertEqual(number_string_to_list('', 1), [])
+        self.assertEqual(number_string_to_list('1', 1), [1])
+        self.assertEqual(number_string_to_list('1', 2), [1])
+        self.assertEqual(number_string_to_list('12', 1), [1, 2])
+        self.assertEqual(number_string_to_list('12', 2), [12])
+        self.assertEqual(number_string_to_list('12', 3), [12])
+        self.assertEqual(number_string_to_list('1234567', 3), [1, 234, 567])
+        self.assertEqual(number_string_to_list('100', 2), [1, 0])
+
+    def test_add_int_lists_with_carry_exception(self):
+        self.assertRaises(ValueError, add_int_lists_with_carry, [1], [2], 0)
+
+    def test_add_int_lists_with_carry_expected(self):
+        self.assertEqual(add_int_lists_with_carry([], [], 1), [])
+        self.assertEqual(add_int_lists_with_carry([1], [], 1), [1])
+        self.assertEqual(add_int_lists_with_carry([9], [1], 1), [1, 0])
+        self.assertEqual(add_int_lists_with_carry([9], [1], 2), [10])
+        self.assertEqual(add_int_lists_with_carry([9, 9], [1], 1), [1, 0, 0])
+        self.assertEqual(add_int_lists_with_carry([9, 9], [1], 1), [1, 0, 0])
+
+
 
 
 if __name__ == '__main__':
