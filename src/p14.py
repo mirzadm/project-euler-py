2e1327161137a7f44befc6099889b4ddbc3d22bd
Mirzad Mohandespour <mirzadm@gmail.com>
2018-07-31 14:54:15 -0500

    problem 14: minor refactoring

--- src/p14.py
+++ src/p14.py
@@ -8,15 +8,21 @@ def calculate_sequence_length(n, length_dict):
         n --> n/2   if n even
         n --> 3n+1  if n odd
         ends        if n is 1
+    
+    Example:
+        Sequence starting at 10:
+            10, 5, 16, 8, 4, 2, 1
+        Corresponding dicitonary:
+            {10: 7, 5: 6, 16: 5, 8: 4, 4: 3, 2: 2, 1: 1}
 
-    Reads and mutates `length_dict` dicitonary in the process.
+    Reads and mutates `length_dict` dicitonary.
     
     Args:
         `n`: Positive integer.
-        `length_dict`: A dictionary of sequence terms and their corresponding
-                       lengths.
+        `length_dict`: A dictionary of previously calculated sequence terms
+                       and their corresponding lengths.
     Returns:
-            Length of the sequence 
+            Length of the sequence starting at `n`
     """
     if n < 1:
         raise ValueError('Invalid input argument.')
@@ -26,8 +32,10 @@ def calculate_sequence_length(n, length_dict):
             length_dict[n] = 1
         else:
             if n % 2 == 0:
-                length_dict[n] = 1 + calculate_length(n // 2, length_dict)
+                length_dict[n] = 1 + calculate_sequence_length(n // 2,
+                                                               length_dict)
             else:
-                length_dict[n] = 1 + calculate_length(3 * n + 1, length_dict)
+                length_dict[n] = 1 + calculate_sequence_length(3 * n + 1,
+                                                               length_dict)
 
     return length_dict[n]
