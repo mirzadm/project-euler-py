a46ca86a236c74285185480b0512e464c1810aad
Mirzad Mohandespour <mirzadm@gmail.com>
2018-07-31 12:19:19 -0500

    problem 14: using a dictionary

--- src/p14.py
+++ src/p14.py
@@ -1,44 +1,26 @@
 """Project Euler: Problem 14."""
 
 
-# def find_longest_sequence(upper_limit):
-#     """Finds the number in 1 to `upper_limit` generating the longest sequence.
+def calculate_sequence_length(n, length_dict):
+    """Calculates the sequence length for `n`.
 
-#     The sequence: 
-#         n --> n/2   if n even
-#         n --> 3n+1  if n odd
-#         ends        if n is 1
-#     """
+    The sequence: 
+        n --> n/2   if n even
+        n --> 3n+1  if n odd
+        ends        if n is 1
 
-#     visited = {}
-#     longest = 0
-#     i = upper_limit
-#     while i > 1:
-#         if i in visited:
-#             i -= 1
-#             continue
-#         else:
+    Uses and updates `length_dict` dicitonary in the process.
+    """
+    if n < 1:
+        raise ValueError('Invalid input argument.')
 
-#             if 
-
-
-def sequence_length(n):
-    k = 1
-    while n > 1:
-        if n % 2 == 0:
-            n = n // 2
+    if n not in length_dict:
+        if n == 1:
+            length_dict[n] = 1
         else:
-            n = 3 * n + 1
-        k += 1
-    return k
+            if n % 2 == 0:
+                length_dict[n] = 1 + calculate_length(n // 2, length_dict)
+            else:
+                length_dict[n] = 1 + calculate_length(3 * n + 1, length_dict)
 
-def find_max(m):
-    max_seq = 0
-    max_num = None
-    for i in range(m, 1, -1):
-        k = sequence_length(i) 
-        if k > max_seq:
-            max_seq = k
-            max_num = i
-    
-    return (max_num, max_seq)
+    return length_dict[n]
