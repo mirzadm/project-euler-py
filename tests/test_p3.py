2ca7dd48a16a5efb74f42cb40f7eb6e437f3f3a4
Mirzad Mohandespour <mirzadm@gmail.com>
2018-08-05 09:20:30 -0700

    Problem 3: update tests.

--- tests/test_p3.py
+++ tests/test_p3.py
@@ -2,35 +2,21 @@
 
 import unittest
 
-from src.p3 import is_prime, prime_numbers_up_to, largest_prime_factor
+from src.p3 import largest_prime_factor
 
 
-class TestLagrestPrimeFactor(unittest.TestCase):
+class TestP3(unittest.TestCase):
 
-    def test_is_prime(self):
-        self.assertFalse(is_prime(10, [2, 3]))
-        self.assertTrue(is_prime(11, [2, 3]))
-        self.assertFalse(is_prime(100, [2, 3, 5, 7]))
+    def test_largest_prime_factor_exception(self):
+        self.assertRaises(ValueError, largest_prime_factor, 1)
 
-    def test_prime_numbers_up_to(self):
-        self.assertEqual(prime_numbers_up_to(2), [2])
-        self.assertEqual(prime_numbers_up_to(3), [2, 3])
-        self.assertEqual(prime_numbers_up_to(10), [2, 3, 5, 7])
-        self.assertEqual(prime_numbers_up_to(19), [2, 3, 5, 7, 11, 13, 17, 19])
-
-
-    def test_largest_prime_factor(self):
+    def test_largest_prime_factor_expected(self):
         self.assertEqual(largest_prime_factor(2), 2)
         self.assertEqual(largest_prime_factor(3), 3)
         self.assertEqual(largest_prime_factor(4), 2)
         self.assertEqual(largest_prime_factor(5), 5)
-        self.assertEqual(largest_prime_factor(6), 3)
-        self.assertEqual(largest_prime_factor(7), 7)
-        self.assertEqual(largest_prime_factor(8), 2)
-        self.assertEqual(largest_prime_factor(9), 3)
-        self.assertEqual(largest_prime_factor(10), 5)
-        self.assertEqual(largest_prime_factor(66), 11)
-        self.assertEqual(largest_prime_factor(67), 67)
+        self.assertEqual(largest_prime_factor(22), 11)
+        self.assertEqual(largest_prime_factor(100), 5)
 
 
 if __name__ == '__main__':
