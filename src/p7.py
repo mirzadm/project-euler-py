c4a73dcde7f15b0f28a7ec0337c51478a28f56e4
Mirzad Mohandespour <mirzadm@gmail.com>
2018-08-07 15:08:16 -0500

    Problem 7: use one function

--- src/p7.py
+++ src/p7.py
@@ -1,39 +1,13 @@
 """Project Euler: Problem 7."""
 
 
-def is_prime(m, primes):
-    """Checks primality of `m` with respect to given `primes`.
-
-    For the function to work properly, `primes` must at least have all the
-    prime numbers up to square root of `m`.
-    
-    Args:
-        `m`: Integer value greater than 1.
-        `primes`: A list of prime numbers. 
-    Raises:
-        ValueError: For `m` less than or equal to 1.
-    """
-    if m <= 1:
-        raise ValueError('Invalid input argument.')
-
-    sq_root = int(m ** 0.5)
-    i = 0
-    while i < len(primes) and primes[i] <= sq_root:
-        if m % primes[i] == 0:
-            return False
-        i += 1
-    return True
-
-
 def nth_prime_number(n):
     """Returns nth prime number.
-    
-    Uses `is_prime`.
 
     Args:
         `n`: Positive integer.
     Raises:
-        ValueError: For `n` less than 1.
+        IndexError: For `n` less than 1.
     """
     if n < 1:
         raise IndexError('Invalid input argument.')
@@ -42,9 +16,18 @@ def nth_prime_number(n):
     candidate = 2
     num_of_primes = 0
     while num_of_primes != n:
-        if is_prime(candidate, primes):
+        i = 0
+        is_prime = True
+        while i < len(primes) and primes[i] <= int(candidate ** 0.5):
+            if candidate % primes[i] == 0:
+                is_prime = False
+                break
+            i += 1
+
+        if is_prime:
             primes.append(candidate)
             num_of_primes += 1
+
         candidate += 1
 
     return primes[-1]
