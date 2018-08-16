97db4888e7dd3064fbd4efd5b0b9b38ad6b97467
Mirzad Mohandespour <mirzadm@gmail.com>
2018-08-16 15:54:36 -0500

    Problem 10: cleanup

--- src/p10.py
+++ src/p10.py
@@ -1,7 +1,5 @@
 """Project Euler: Problem 10."""
 
-from math import sqrt
-
 
 def sum_of_primes(upper_limit):
     """Finds sum of all prime numbers < `upper_limit`.
@@ -12,28 +10,25 @@ def sum_of_primes(upper_limit):
         `upper_limit`: Positive integer. 
     Returns:
         Sum of all prime numbers less than `upper_limit`. 
-        0 if `upper_limit` less than 3.
     Raises:
         ValueError: If `upper_limit` less than 1.
     """
     if upper_limit < 1:
         raise ValueError('Input argument must be greater than or equal to 1.')
     
-    # Default to prime
-    # Numbers 0 and 1 are also included here but just to simplify
-    # index-number relation. They are not actually used.
-    is_prime_array = [True] * upper_limit  
+    primes = [True] * upper_limit
 
-    upper_limit_sqrt = int(sqrt(upper_limit))
+    upper_limit_sqrt = int(upper_limit ** 0.5)
     
-    for n in range(2, upper_limit_sqrt + 1):
-        if is_prime_array[n]:
-            for i in range(2 * n, upper_limit, n):
-                is_prime_array[i] = False
+    primes_sum = 0
+    for n in range(2, upper_limit_sqrt+1):
+        if primes[n]:
+            primes_sum += n
+            for i in range(2*n, upper_limit, n):
+                primes[i] = False
     
-    total = 0
-    for n in range(2, upper_limit):
-        if is_prime_array[n]:
-            total += n
+    for n in range(upper_limit_sqrt+1, upper_limit):
+        if primes[n]:
+            primes_sum += n
 
-    return total
+    return primes_sum
