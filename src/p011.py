94581d8e30f026c6a2bdc3962db185cc273b623b
Mirzad Mohandespour <mirzadm@gmail.com>
2019-01-02 21:50:48 -0800

    Problem 11: make easier to read

--- src/p011.py
+++ src/p011.py
@@ -1,55 +1,56 @@
 """Project Euler: Problem 11."""
 
 
-def max_adj_prod(grid, num_of_adj):
+def grid_max_adjacent_elements_product(grid, adj_size):
     """Finds max product of adjacent numbers in a grid.
 
-    Finds max product of `num_of_adj` adjacent numbers in `grid`.
+    Finds max product of `adj_size` adjacent numbers in `grid`.
     Adjacency can be horizontal, vertical or diagonal (left or right).
 
     Args:
         `grid`: A non-empty two dimentional matrix of non-negative integers.
-        `num_of_adj`: Positive integer value.
+        `adj_size`: Positive integer value.
     Returns:
         The value of max product if greater than 0. Otherwise 0.
     Raises:
-        ValueError: Invalid `num_of_adj` or empty `grid`.
+        ValueError: Invalid `adj_size` or empty `grid`.
     """
-    rows = len(grid)
-    cols = len(grid[0])
-    if (num_of_adj > cols and num_of_adj > rows) or num_of_adj < 1:
+    row_size = len(grid)
+    col_size = len(grid[0])
+    if (adj_size > col_size and adj_size > row_size) or adj_size < 1:
         raise ValueError('Invalid input argument.')
 
-    max_product = 0
-    for i in range(rows):
-        for j in range(cols):
-            # Horizontal adjacency
-            product = grid[i][j]
-            if j + num_of_adj <= cols:
-                for k in range(1, num_of_adj):
-                    product *= grid[i][j + k]
-                if product > max_product:
-                    max_product = product
-            # Vertical adjacency
-            product = grid[i][j]
-            if i + num_of_adj <= rows:
-                for k in range(1, num_of_adj):
-                    product *= grid[i + k][j]         
-                if product > max_product:
-                    max_product = product
-            # Diagonal (\) adjacency
-            product = grid[i][j]
-            if (i + num_of_adj <= rows) and (j + num_of_adj <= cols):
-                for k in range(1, num_of_adj):
-                    product *= grid[i + k][j + k]            
-                if product > max_product:
-                    max_product = product
-            # Diagonal (/) adjacency
-            product = grid[i][j]
-            if (i + num_of_adj <= rows) and (j >= num_of_adj - 1):
-                for k in range(1, num_of_adj):
-                    product *= grid[i + k][j - k]            
-                if product > max_product:
-                    max_product = product
+    current_global_max = 0
+    for row in range(row_size):
+        for col in range(col_size):
+            next_local_max = _local_max_product(grid, adj_size, row_size,
+                                                col_size, row, col)
+            if next_local_max > current_global_max:
+                current_global_max = next_local_max
+    return current_global_max
 
-    return max_product
+
+def _local_max_product(grid, adj_size, row_size, col_size, row, col):
+    products = [0] * 4
+    # Horizontal adjacency
+    if col + adj_size <= col_size:
+        products[0] = grid[row][col]
+        for k in range(1, adj_size):
+            products[0] *= grid[row][col + k]
+    # Vertical adjacency
+    if row + adj_size <= row_size:
+        products[1] = grid[row][col]
+        for k in range(1, adj_size):
+            products[1] *= grid[row + k][col]         
+    # Diagonal (\) adjacency
+    if (row + adj_size <= row_size) and (col + adj_size <= col_size):
+        products[2] = grid[row][col]
+        for k in range(1, adj_size):
+            products[2] *= grid[row + k][col + k]            
+    # Diagonal (/) adjacency
+    if (row + adj_size <= row_size) and (col >= adj_size - 1):
+        products[3] = grid[row][col]
+        for k in range(1, adj_size):
+            products[3] *= grid[row + k][col - k]            
+
+    return max(products)
