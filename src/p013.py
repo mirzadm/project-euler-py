fa14301bdcae0cfb7a0fe1db49204a5154bbd8ab
Mirzad Mohandespour <mirzadm@gmail.com>
2019-07-20 10:19:06 -0500

    Problem 13: rewrite to use strings

--- src/p013.py
+++ src/p013.py
@@ -1,115 +1,56 @@
 """Project Euler: Problem 13."""
 
 
-def string_number_to_list(number_string, digit_width):
-    """Converts a string-formatted number to a list of its sub-digits.
-
-    Starting from right, slices the string to `digit_width` size pieces.
-    Then converts each slice to an integer and stores it in a list.
-    Example:
-        `number_string` : '12345'
-        `digit_width` : 2
-         returns: [1, 23, 45]
+def sum_str_nums(str_num1, str_num2, step=10):
+    """Sums up two long string formatted integers.
+    
+    Adds up two string formatted integers iteratively, `step` digits at a time.
 
     Args:
-        `number_string`: A string-formatted unsigned integer.
-        `digit_width`: Positive integer.
-    Returns:
-        A list of unsigned integers.
-    Raises:
-        ValueError: For `digit_width` less than 1.
+        `str_num1`, `str_num2`: String formatted unsigned integers.
+        `step`: Positive integer.
+    Return:
+        String formatted sum.
     """
-    if digit_width < 1:
-        raise ValueError('Invalid input argument value.')
-
-    result = []
-    i = len(number_string)
-    while i > digit_width:
-        next_slice = number_string[i - digit_width:i]
-        result.insert(0, int(next_slice))
-        i -= digit_width
-    if i > 0:
-        next_slice = number_string[:i]
-        result.insert(0, int(next_slice))
-
-    return result
-
-
-def add_int_lists_with_carry(first_int_list, second_int_list, digit_width):
-    """Adds two lists of integers from right to left with carry.
-
-    Assumes each integer has no more than `digit_width` digits. 
-
-    Example:
-        `digit_width`:                   2
-        `first_int_list`:   [1,  0, 99] --> 10099
-        `second_int_list`:     [99,  1] -->  9901  
-        returns:            [2,  0,  0] --> 20000
-
-    Args:
-        `first_int_list`, `second_int_list`: Lists of unsigned integers with
-            no more than `digit_width` digits.
-        `digit_width`: Positive integer.
-    Returns:
-        A list of integers.
-    Raises:
-        ValueError: For `digit_width` less than 1.
-    """
-    if digit_width < 1:
-        raise ValueError('Invalid input argument value.')
-    # Identify longer and shorter number lists
-    long_num, short_num = first_int_list, second_int_list
-    if len(long_num) < len(short_num):
-        long_num, short_num = short_num, long_num
-
-    result = []
-    short_index = len(short_num) - 1
-    long_index = len(long_num) - 1
-    power_of_10 = 10 ** digit_width
+    if step <= 0:
+        raise ValueError("`step` must be a positive integers.")
+    right1 = len(str_num1)
+    right2 = len(str_num2)
     carry = 0
-    while short_index >=0:
-        new_sum = long_num[long_index] + short_num[short_index] + carry
-        carry = new_sum // power_of_10
-        result.insert(0, new_sum % power_of_10)
-        short_index -= 1
-        long_index -= 1
+    result = ""
+    step_power = 10 ** step
+    while right1 > 0 or right2 > 0:
+        if right1 >= step:
+            op1 = int(str_num1[right1 - step: right1])
+            right1 -= step
+        elif right1 > 0:
+            op1 = int(str_num1[0: right1])
+            right1 = 0
+        else:
+            op1 = 0
 
-    while long_index >= 0:
-        new_sum = long_num[long_index] + carry
-        carry = new_sum // power_of_10
-        result.insert(0, new_sum % power_of_10)
-        long_index -= 1
+        if right2 >= step:
+            op2 = int(str_num2[right2 - step: right2])
+            right2 -= step
+        elif right2 > 0:
+            op2 = int(str_num2[0: right2])
+            right2 = 0
+        else:
+            op2 = 0
 
-    if carry > 0:
-        result.insert(0, carry)
-
+        next_sum = op1 + op2 + carry
+        carry = next_sum // step_power
+        next_step_sum = next_sum % step_power
+        if right1 > 0 or right2 > 0:
+            result = str(next_step_sum).zfill(step) + result
+        else:
+            result = str(next_sum) + result
     return result
 
 
-def add_up_string_numbers(string_numbers, digit_width):
-    """Adds a list of string numbers.
-
-    The sum is calculated `digit_width` digits at a time.
-
-    Args:
-        `string_numbers`: A list of string-formatted unsigned integers.
-        `digit_width`: Positive integer.
-    Returns:
-        A string representation of sum value.
-    Raises:
-        ValueError: For `digit_width` less than 1.
-    """
-    if digit_width < 1:
-        raise ValueError('Invalid input argument value.')
-
-    sum_array = []
-    for string_num in string_numbers:
-        num_array = string_number_to_list(string_num, digit_width)
-        sum_array =  add_int_lists_with_carry(sum_array, num_array,
-                                              digit_width)
-    sum_string = ''
-    if len(sum_array) > 0:
-        sum_string = str(sum_array[0])
-        sum_string += ''.join(str(sum_array[i]).zfill(digit_width) for i in
-                              range(1, len(sum_array)))
-    return sum_string
+def add_list_str_nums(str_nums):
+    """A wrapper to solve the main problem with."""
+    result = str_nums[0]
+    for str_num in str_nums[1: ]:
+        result = sum_str_nums(result, str_num)
+    return result
--- tests/test_p013.py
+++ tests/test_p013.py
@@ -1,51 +1,35 @@
-"""Unit tests for p13.py."""
+"""Unit tests for p013."""
 
 import unittest
 
-from src.p013 import (string_number_to_list, add_int_lists_with_carry,
-                     add_up_string_numbers)
+from src.p013 import sum_str_nums
 
 
-class TestP13(unittest.TestCase):
-
-    def test_string_number_to_list_exception(self):
-        self.assertRaises(ValueError, string_number_to_list, '123', 0)
-
-    def test_string_number_to_list_expected(self):
-        self.assertEqual(string_number_to_list('', 1), [])
-        self.assertEqual(string_number_to_list('1', 1), [1])
-        self.assertEqual(string_number_to_list('1', 2), [1])
-        self.assertEqual(string_number_to_list('12', 1), [1, 2])
-        self.assertEqual(string_number_to_list('12', 2), [12])
-        self.assertEqual(string_number_to_list('12', 3), [12])
-        self.assertEqual(string_number_to_list('1234567', 3), [1, 234, 567])
-        self.assertEqual(string_number_to_list('100', 2), [1, 0])
-
-    def test_add_int_lists_with_carry_exception(self):
-        self.assertRaises(ValueError, add_int_lists_with_carry, [1], [2], 0)
-
-    def test_add_int_lists_with_carry_expected(self):
-        self.assertEqual(add_int_lists_with_carry([], [], 1), [])
-        self.assertEqual(add_int_lists_with_carry([1], [], 1), [1])
-        self.assertEqual(add_int_lists_with_carry([9], [1], 1), [1, 0])
-        self.assertEqual(add_int_lists_with_carry([9], [1], 2), [10])
-        self.assertEqual(add_int_lists_with_carry([9, 9], [1], 1), [1, 0, 0])
-        self.assertEqual(add_int_lists_with_carry([9, 9], [1], 1), [1, 0, 0])
-
-    def test_add_up_string_numbers_exception(self):
-        self.assertRaises(ValueError,add_up_string_numbers, ['123'], 0)
-
-    def test_add_up_string_numbers_expected(self):
-        self.assertEqual(add_up_string_numbers([], 1), '')
-        self.assertEqual(add_up_string_numbers(['0'], 1), '0')
-        self.assertEqual(add_up_string_numbers(['0', '1'], 1), '1')
-        self.assertEqual(add_up_string_numbers(['9', '1'], 1), '10')
-        self.assertEqual(add_up_string_numbers(['9', '1'], 2), '10')
-        self.assertEqual(add_up_string_numbers(['99', '1'], 1), '100')
-        self.assertEqual(add_up_string_numbers(['99', '1'], 2), '100')
-        self.assertEqual(add_up_string_numbers(['99', '1'], 3), '100')
-        self.assertEqual(add_up_string_numbers(['99', '99', '1'], 2), '199')
-
-
-if __name__ == '__main__':
-    unittest.main()
+class TestP013(unittest.TestCase):
+    def test_sum_str_nums(self):
+        step = 0
+        self.assertRaises(ValueError, sum_str_nums, "12", "34", step)
+        step = 1
+        self.assertEqual(sum_str_nums("0", "0", step), "0")
+        self.assertEqual(sum_str_nums("1", "1", step), "2")
+        self.assertEqual(sum_str_nums("9", "1", step), "10")
+        self.assertEqual(sum_str_nums("10", "0", step), "10")
+        self.assertEqual(sum_str_nums("10", "1", step), "11")
+        self.assertEqual(sum_str_nums("99", "1", step), "100")
+        self.assertEqual(sum_str_nums("99", "99", step), "198")
+        self.assertEqual(sum_str_nums("100", "99", step), "199")
+        self.assertEqual(sum_str_nums("100", "99", step), "199")
+        self.assertEqual(sum_str_nums("100", "99", step), "199")
+        self.assertEqual(sum_str_nums("101", "99", step), "200")
+        step = 10
+        self.assertEqual(sum_str_nums("0", "0", step), "0")
+        self.assertEqual(sum_str_nums("1", "1", step), "2")
+        self.assertEqual(sum_str_nums("9", "1", step), "10")
+        self.assertEqual(sum_str_nums("10", "0", step), "10")
+        self.assertEqual(sum_str_nums("10", "1", step), "11")
+        self.assertEqual(sum_str_nums("99", "1", step), "100")
+        self.assertEqual(sum_str_nums("99", "99", step), "198")
+        self.assertEqual(sum_str_nums("100", "99", step), "199")
+        self.assertEqual(sum_str_nums("100", "99", step), "199")
+        self.assertEqual(sum_str_nums("100", "99", step), "199")
+        self.assertEqual(sum_str_nums("101", "99", step), "200")
